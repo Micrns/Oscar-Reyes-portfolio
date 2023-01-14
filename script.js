069cc178f9b25491ad0c9b5b4563e23e7fda3677
Oscar Reyes <97998440+Micrns@users.noreply.github.com>
2023-01-14 01:19:02 -0800

    text-animation

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ window.onload = () =>{
     const transition_el = document.querySelector('.transition')
     setTimeout(()=> {transition_el.classList.remove('is-active')}, 2000);
   }
+
+
+  const text = document.querySelector(".second-text");
+
+  const textLoad =  () =>{
+    setTimeout(() => {text.textContent = "Software Engineer";}, 0);
+    setTimeout(() => {text.textContent = "graduate of UCI";}, 4000);
+    setTimeout(() => {text.textContent = "visionary";}, 8000);
+
+  }
+
+  textLoad();
+  setInterval(textLoad, 12000);
