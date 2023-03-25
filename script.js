b6cfaba03f92c73b00122691ee4a04563b9207fb
Oscar Reyes <97998440+Micrns@users.noreply.github.com>
2023-03-25 02:29:21 -0700

    changes to index

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const text = document.querySelector(".second-text");
 const textLoad =  () =>{
   setTimeout(() => {text.textContent = "Software Engineer";}, 0);
   setTimeout(() => {text.textContent = "graduate of UCI";}, 4000);
-  setTimeout(() => {text.textContent = "visionary";}, 8000);
+  setTimeout(() => {text.textContent = "Learner";}, 8000);
 
 }
 
